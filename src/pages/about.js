fea0678038339249df35b9077ec4dd51fc7e1829
Shub4am <shub4am@gmail.com>
2023-03-31 00:48:55 +0530

    updated about section

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
+    <div className="">
       <div className="flex justify-center items-center">
         <button className="relative mt-24 bg-green-200  hover:bg-green-600 px-4 py-1 rounded-2xl text-green-500 hover:text-white h-8 w-28 font-black text-xs cursor-pointer ">
           Our Story
         </button>
       </div>
 
-      <div className="relative">
+      <div className="relative ">
         <div className="flex flex-col justify-center items-center mt-20">
           <p className="text-10xl text-blue-700 opacity-5 font-black text-center z-0 absolute top-0 left-0 w-full">
             OUR STORY
@@ -31,6 +31,33 @@ const About = () => {
           interest vested in this (also) an item of real property,
           <br /> (more generally) buildings or housing in general.
         </p>
+      </div>
+
+      <div className="flex justify-center items-center mt-[50px]">
+        <div className="bg-white py-35 px-10 h-[160px] w-[350px] mr-[30px] flex flex-col justify-center items-center rounded-[10px] shadow-md">
+          <p className="font-medium text-3xl leading-[50px] text-[#4D5E80] capitalize">
+            15 years
+          </p>
+          <p className="text-sm leading-8 text-center text-[#ADB8CC]">
+            in business
+          </p>
+        </div>
+        <div className="bg-white py-35 px-10 h-[160px] w-[350px] mr-[30px] flex flex-col justify-center items-center rounded-[10px] shadow-md">
+          <p className="font-medium text-3xl leading-[50px] text-[#4D5E80] capitalize">
+            $1 billion
+          </p>
+          <p className="text-sm leading-8 text-center text-[#ADB8CC]">
+            property brokered
+          </p>
+        </div>
+        <div className="bg-white py-35 px-10 h-[160px] w-[350px] mr-[30px] flex flex-col justify-center items-center rounded-[10px] shadow-md">
+          <p className="font-medium text-3xl leading-[50px] text-[#4D5E80] capitalize">
+            10,000
+          </p>
+          <p className="text-sm leading-8 text-center text-[#ADB8CC]">
+            transactions
+          </p>
+        </div>
       </div>
     </div>
   );
